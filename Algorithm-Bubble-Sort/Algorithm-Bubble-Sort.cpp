531037552af9918e41f47f3618a73072bd12c635
Aditya Pandey <pandey.aditya2006@gmail.com>
2024-08-20 13:48:13 +0800

    Implemented reduced passes optimization.

--- Algorithm-Bubble-Sort/Algorithm-Bubble-Sort.cpp
+++ Algorithm-Bubble-Sort/Algorithm-Bubble-Sort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void printarray(int array_name[], int size); // Function to print the array by iterating over it 
 void generate_array(int array_name[], int size); // Initialize an random array of size <size>
-void bubble_sort(int array_name[], int size); // Sort the array using the Bubble Sort Algorithm
+void bubble_sort(int array_name[], int size); // Sort the array using the Bubble Sort Algorithm - Worst Case time complexity of O(n^2)
 
 int main(){
     srand(time(nullptr)); // Setting seed as current time from Jan 1, 1970 in seconds so random numbers are different each time 
@@ -34,15 +34,17 @@ void generate_array(int array_name[], int size){
 void bubble_sort(int array_name[], int size){
     bool swap;
     int passes=0;
+    int length_to_check=0;
     do{
         swap = false;
-        for(int j=0; j<size-1; j++){
+        for(int j = 0; j < size-1; j++){
             if(array_name[j] > array_name[j+1]){
                 int temp = array_name[j];
                 array_name[j] = array_name[j+1];
                 array_name[j+1] = temp;
                 swap = true;
             }}
+        length_to_check++;
         printarray(array_name, 10);
         passes++;
     } while (swap==true);
